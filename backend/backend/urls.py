c750acea6b70293c30e914183bc150e7e0ca6c74
artfred <artfred20202020@gmail.com>
2023-01-05 15:10:22 +0800

    a

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,6 +5,8 @@ from django.contrib import admin
 from django.urls import path, include                 # add this
 from rest_framework import routers                    # add this
 from todo import views                            # add this
+from django.conf.urls.static import static
+from django.conf import settings
         
 router = routers.DefaultRouter()                      # add this
 router.register(r'todos', views.TodoView, 'todo')     # add this
@@ -13,3 +15,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),           
     path('api/', include(router.urls))                # add this
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
---
+++ frontend/build/precache-manifest.bfc5ab009fd82f00c89f0ad001d160a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f86d44360ef54126f1e9",
+    "url": "/api/todos/static/css/main.ff127bf5.chunk.css"
+  },
+  {
+    "revision": "f86d44360ef54126f1e9",
+    "url": "/api/todos/static/js/main.f86d4436.chunk.js"
+  },
+  {
+    "revision": "6b485d4848892890241f",
+    "url": "/api/todos/static/css/1.8952187c.chunk.css"
+  },
+  {
+    "revision": "6b485d4848892890241f",
+    "url": "/api/todos/static/js/1.6b485d48.chunk.js"
+  },
+  {
+    "revision": "69cf9931b6841d3ebe20",
+    "url": "/api/todos/static/js/runtime~main.69cf9931.js"
+  },
+  {
+    "revision": "9507cdd0ae20b75a05e1b163695efe29",
+    "url": "/api/todos/index.html"
+  }
+];
